91ad3232d502236ac1aad0e0bb8c17fd2d67df03
RexDude <rexdude@arcor.de>
2014-02-01 11:02:36 +0100

    Added alert on module update

--- pages/image_module.inc.php
+++ pages/image_module.inc.php
@@ -52,7 +52,7 @@ if (isset($_REQUEST["install"]) && $_REQUEST["install"] == 1) {
 		<?php
 			if ($module_id > 0) {
 				if (!isset($_REQUEST["install"])) {
-					echo '<li><a href="index.php?page=magnific_popup&amp;subpage=image_module&amp;install=1&amp;module_id=' . $module_id . '">' . $I18N->msg('magnific_popup_module_update') . '</a></li>';
+					echo '<li><a id="update-module" href="index.php?page=magnific_popup&amp;subpage=image_module&amp;install=1&amp;module_id=' . $module_id . '">' . $I18N->msg('magnific_popup_module_update') . '</a></li>';
 				}
     		} else {
 				if (!isset($_REQUEST["install"])) {
@@ -66,4 +66,14 @@ if (isset($_REQUEST["install"]) && $_REQUEST["install"] == 1) {
 		<p class="headline"><?php echo $I18N->msg('magnific_popup_module_output'); ?></p><?php rex_highlight_string($moduleOutput); ?>
 	</div>
 </div>
+
+<script type="text/javascript">
+jQuery(document).ready( function() {
+	jQuery('a#update-module').click(function(e) {
+		if (!confirm("<?php echo $I18N->msg('ckeditor_module_update_msg'); ?>")) {
+			e.preventDefault();
+		}
+	});
+});
+</script>
 
